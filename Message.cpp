2b4a3e49ddfc32bda1b09d7327a38b07dac06a61
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2025-03-05 22:30:01 +0530

    Create Message.cpp

---
+++ Message.cpp
@@ -0,0 +1,16 @@
+// Program to Demonstrate Working of function with no return type and no parameter 
+// OOPS features(namespace,class,object,access specifier,memeber variables, and memeber functions)
+#include<iostream>
+using namespace std;
+class program
+{
+    public: void message()
+    {
+        cout <<"Hi";
+    }
+};
+int main(){
+    program obj;
+    obj.message();
+    return 0;
+}
