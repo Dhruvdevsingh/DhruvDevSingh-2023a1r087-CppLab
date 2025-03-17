79f8410b362de917ad87073f84237e4fa38e4d0f
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2025-03-17 06:56:54 +0530

    Create Experiment1a.cpp

---
+++ Experiment1a.cpp
@@ -0,0 +1,16 @@
+// Analyze and Design classes for a student management system
+// Classes for Student Mangement System:-
+//1.Student Class
+// To Store and display Student Information
+//2.Course Class
+// To Store and display Course Details
+//3.Admin Class
+// To change /Add/Delete/Update,information of Student or Course
+//4.Examination
+// To Store and display information about the ongoing or upcomming exams.
+//5.Result Class
+//To store and display Result.
+//6.Department Class
+// To Store and Display information of all Schols and Department of College.
+//7. Notification Class
+// To Display all latest information and Updates.
